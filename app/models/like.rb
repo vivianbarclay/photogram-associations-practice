151f4f136732cefe4be11d6ef209c002de99b04d
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-19 21:32:26 +0000

    giving up for now because i cant find anything wrong and this is irritating me

--- app/models/like.rb
+++ app/models/like.rb
@@ -14,6 +14,6 @@ class Like < ApplicationRecord
   validates :photo_id, presence: true, uniqueness: { scope: :fan_id }
 
   # Direct Associations
+  belongs_to :photo, inverse_of: :likes, required: true
   belongs_to :fan, class_name: "User", foreign_key: "fan_id"
-  belongs_to :photo, required: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,9 +16,13 @@ class Photo < ApplicationRecord
   validates :owner_id, presence: true
 
   # Direct Associations
-  belongs_to :poster, class_name: "User", foreign_key: "owner_id"
+  belongs_to :poster, class_name: "User", foreign_key: "owner_id", inverse_of: :own_photos
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  # Ensure `comments_count` and `likes_count` automatically update
+  has_many :comments, dependent: :destroy, counter_cache: true
+  has_many :likes, dependent: :destroy, counter_cache: true
 
   # Indirect Associations
   has_many :fans, through: :likes, source: :fan
